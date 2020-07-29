0156738111a38587f671fcda2fae883c646f56bf
Benjamin Heisch <benjaminheisch@yahoo.de>
2020-07-29 15:27:09 +0200

    eddited stuff, so it works on debian. Also added codecoverage on tests

--- scripts/test_cov.sh
+++ scripts/test_cov.sh
@@ -1,3 +1,6 @@
 #!/bin/
-
-cmake -E env CXXFLAGS="-g -O0 -Wall -W -Wshadow -Wunused-variable -Wunused-parameter -Wunused-function -Wunused -Wno-system-headers -Wno-deprecated -Woverloaded-virtual -Wwrite-strings -fprofile-arcs -ftest-coverage" C_FLAGS="-g -O0 -Wall -W -fprofile-arcs -ftest-coverage" LDFLAGS="-fprofile-arcs -ftest-coverage" cmake ..
+mkdir build
+cd build
+cmake -E env CXXFLAGS="-g -O0 -Wall -W -Wshadow -Wunused-variable -Wunused-parameter -Wunused-function -Wunused -Wno-system-headers -Wno-deprecated -Woverloaded-virtual -Wwrite-strings -fprofile-arcs -ftest-coverage" C_FLAGS="-g -O0 -Wall -W -fprofile-arcs -ftest-coverage" LDFLAGS="-fprofile-arcs -ftest-coverage -lgcov" cmake ..
+ctest .
+gcovr --fail-under-line 80 -s  --exclude-directories "test|examples"  -r /mnt/c/Users/Benjamin/Source/Repos/xplug/  .c
