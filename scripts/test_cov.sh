281513d3de39384af68d646889e312d774b72b9f
Benjamin Heisch <benjaminheisch@yahoo.de>
2020-08-04 12:45:10 +0200

    updated test cov script

--- scripts/test_cov.sh
+++ scripts/test_cov.sh
@@ -1,6 +1,6 @@
-#!/bin/
+#!/bin/sh
 mkdir build
 cd build
-cmake -E env CXXFLAGS="-g -O0 -Wall -W -Wshadow -Wunused-variable -Wunused-parameter -Wunused-function -Wunused -Wno-system-headers -Wno-deprecated -Woverloaded-virtual -Wwrite-strings -fprofile-arcs -ftest-coverage" C_FLAGS="-g -O0 -Wall -W -fprofile-arcs -ftest-coverage" LDFLAGS="-fprofile-arcs -ftest-coverage -lgcov" cmake ..
+cmake -E env CXXFLAGS="-g -O0 -Wall -W -Wshadow -Wunused-variable -Wunused-parameter -Wunused-function -Wunused -Wno-system-headers -Wno-deprecated -Woverloaded-virtual -Wwrite-strings -fprofile-arcs -ftest-coverage" C_FLAGS="-g -O0 -Wall -W -fprofile-arcs -ftest-coverage" LDFLAGS="-fprofile-arcs -ftest-coverage -lgcov" cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Debug ..
 ctest .
-gcovr --fail-under-line 80 -s  --exclude-directories "test|examples"  -r /mnt/c/Users/Benjamin/Source/Repos/xplug/  .
+gcovr --fail-under-line 80 -s -j4  -r $(pwd)/../../  $gcovr --fail-under-line 80 -s -e '.*/deps/.*$' -e '.*(CMake|(T|t)tl|string_view).*$'  --exclude-directories "test/|examples/|deps/"   -r $(pwd)/..  $(pwd)
