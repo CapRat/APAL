b2b85fd931e77e352ac258078f4b097d2c8c6eba
Benjamin Heisch <benjaminheisch@yahoo.de>
2020-08-04 14:00:07 +0200

    Some changes on test_cov.sh

--- scripts/test_cov.sh
+++ scripts/test_cov.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mkdir build
 cd build
-cmake -E env CXXFLAGS="-g -O0 -Wall -W -Wshadow -Wunused-variable -Wunused-parameter -Wunused-function -Wunused -Wno-system-headers -Wno-deprecated -Woverloaded-virtual -Wwrite-strings -fprofile-arcs -ftest-coverage" C_FLAGS="-g -O0 -Wall -W -fprofile-arcs -ftest-coverage" LDFLAGS="-fprofile-arcs -ftest-coverage -lgcov" cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Debug ..
+cmake -E env CXXFLAGS="-g -O0 -Wall -W -Wshadow -Wunused-variable -Wunused-parameter -Wunused-function -Wunused -Wno-system-headers -Wno-deprecated -Woverloaded-virtual -Wwrite-strings -fprofile-arcs -ftest-coverage" C_FLAGS="-g -O0 -Wall -W -fprofile-arcs -ftest-coverage" LDFLAGS="-fprofile-arcs -ftest-coverage -lgcov" cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Debug ../..
+cmake --build . 
 ctest .
 gcovr --fail-under-line 80 -s -j4  -r $(pwd)/../../  $gcovr --fail-under-line 80 -s -e '.*/deps/.*$' -e '.*(CMake|(T|t)tl|string_view).*$'  --exclude-directories "test/|examples/|deps/"   -r $(pwd)/..  $(pwd)
